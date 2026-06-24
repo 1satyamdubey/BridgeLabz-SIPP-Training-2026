fb9e180867db965ebf6999803f7d3097209cd3e7
satyam dubey <king8445512951@gmail.com>
2026-06-24 12:11:37 +0530

    Add library management system

---
+++ core-java-practice/gcr-codebase/programming-elements/management.java
@@ -0,0 +1,96 @@
+abstract class LibraryItem {
+    private int itemId;
+    private String title;
+    private String author;
+
+    public LibraryItem(int itemId, String title, String author) {
+        this.itemId = itemId;
+        this.title = title;
+        this.author = author;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public abstract int getLoanDuration();
+
+    public void displayItemDetails() {
+        System.out.println("Item ID: " + itemId);
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Loan Duration: " + getLoanDuration() + " days");
+    }
+}
+
+class Book extends LibraryItem {
+    public Book(int itemId, String title, String author) {
+        super(itemId, title, author);
+    }
+
+    @Override
+    public int getLoanDuration() {
+        return 14;
+    }
+}
+
+class Magazine extends LibraryItem {
+    public Magazine(int itemId, String title, String author) {
+        super(itemId, title, author);
+    }
+
+    @Override
+    public int getLoanDuration() {
+        return 7;
+    }
+}
+
+class DVD extends LibraryItem {
+    public DVD(int itemId, String title, String author) {
+        super(itemId, title, author);
+    }
+
+    @Override
+    public int getLoanDuration() {
+        return 3;
+    }
+}
+
+public class management {
+    public static void main(String[] args) {
+        LibraryItem book = new Book(101, "The Hobbit", "J.R.R. Tolkien");
+        LibraryItem magazine = new Magazine(202, "National Geographic", "Various");
+        LibraryItem dvd = new DVD(303, "Inception", "Christopher Nolan");
+
+        System.out.println("Library Management System");
+        System.out.println("-------------------------");
+
+        book.displayItemDetails();
+        System.out.println();
+
+        magazine.displayItemDetails();
+        System.out.println();
+
+        dvd.displayItemDetails();
+    }
+}
